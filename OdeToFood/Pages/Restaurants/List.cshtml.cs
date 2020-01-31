6affb177e0d5192ef0aebbee8cdf399ef0609db0
Vishal Khanapure <kvishal@ufl.edu>
2020-01-31 12:25:39 -0500

    Added display of dummy restaurant data to the Restuarants/List page. Fixed an MVC routing issue in startup.

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,23 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
         
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
         
         public void OnGet()
         {
-//            Message = "Hello, World!";
             Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -31,7 +31,8 @@ namespace OdeToFood
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+//            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
