77dfbbf520a7c531a6aee5e6e0c35a443a856fe5
Vishal Khanapure <kvishal@ufl.edu>
2020-02-18 18:14:24 -0500

    Added Edit Restaurant page and model. Made the restaurant search case insensitive.

--- OdeToFood.Data/InMemoryRestaurantData.cs
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using OdeToFood.Core;
 
@@ -12,18 +13,19 @@ namespace OdeToFood.Data
             restaurants = new List<Restaurant>()
             {
                 new Restaurant() {Id = 1, Name = "V Pizza", Location = "Texas", Cuisine = CuisineType.Italian},
-                new Restaurant() {Id = 2, Name = "A Tacos", Location = "California", Cuisine = CuisineType.Mexican},
+                new Restaurant() {Id = 2, Name = "Amazing Tacos", Location = "California", Cuisine = CuisineType.Mexican},
                 new Restaurant() {Id = 3, Name = "Fifth Element", Location = "Florida", Cuisine = CuisineType.Indian},
                 new Restaurant() {Id = 4, Name = "Amrit Palace", Location = "Texas", Cuisine = CuisineType.Indian},
                 new Restaurant() {Id = 5, Name = "Chopstix Bistro", Location = "Florida", Cuisine = CuisineType.Chinese},
                 new Restaurant() {Id = 6, Name = "Bangkok Square", Location = "New York", Cuisine = CuisineType.Thai},
+                new Restaurant() {Id = 6, Name = "Olive Garden", Location = "New York", Cuisine = CuisineType.Italian},
             };
         }
 
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   where string.IsNullOrEmpty(name) || r.Name.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0
                    orderby r.Name
                    select r;
         }
